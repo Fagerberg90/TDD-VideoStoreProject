a8ac1d54cd4e980897a6c9a0b13d8e7b7aee9ecc
henrikbernhardsson <henke.ber@gmail.com>
2017-05-25 10:48:36 +0200

    Build customer class and movie class

--- TDD-VideoStoreProject/VideoStore/Customer.cs
+++ TDD-VideoStoreProject/VideoStore/Customer.cs
@@ -8,5 +8,16 @@ namespace VideoStore
 {
    public class Customer
     {
+        public string Name { get; set; }
+        public string Ssn { get; set; }
+        public Customer(string name, string ssn)
+        {
+            this.Name = name;
+            this.Ssn = ssn;
+        }
+        public Customer()
+        {
+
+        }
     }
 }
--- TDD-VideoStoreProject/VideoStore/Movie.cs
+++ TDD-VideoStoreProject/VideoStore/Movie.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace VideoStore
 {
+
+    public enum MovieGenre
+    {
+        Comedy, Action, Thriller, Documentary
+    }
     public class Movie
     {
+        public string Title { get; set; }
+        public MovieGenre Genre { get; set; }
+        public Movie(string title, MovieGenre genre)
+        {
+            this.Title = title;
+            this.Genre = genre;
+        }
+        public Movie() { }
     }
 }
