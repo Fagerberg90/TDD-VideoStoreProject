d2fdbe29c8cbd84c4d9d718accfc9f8844995eee
Emil <fagerberg_90@hotmail.com>
2017-05-25 10:29:54 +0200

    Added som classes and implement the interface to two classes

---
+++ TDD-VideoStoreProject/VideoStore/Rental.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoStore
+{
+    public class Rental : IRentals
+    {
+        public void AddRental(string movieTitle, string socialSecurityNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Rental> GetRentalsFor(string socialSecurityNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRental(string movieTitle, string socialSecurityNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
