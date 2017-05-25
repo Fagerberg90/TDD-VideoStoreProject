6e89777f2f4466a9e6d8e5611ecbae08a9d13a85
Emil <fagerberg_90@hotmail.com>
2017-05-25 10:20:52 +0200

    Added two interfaces in BL

---
+++ TDD-VideoStoreProject/VideoStore/Interfaces.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoStore
+{
+    public interface IVideoStore
+    {
+        void RegisterCustomer(string name, string socialSecurityNumber);
+        void AddMovie(Movie movie);
+        void RentMovie(string movieTitle, string socialSecurityNumber);
+        List<Customer> GetCustomers();
+        void ReturnMovie(string movieTitle, string socialSecurityNumber);
+    }
+    public interface IRentals
+    {
+        void AddRental(string movieTitle, string socialSecurityNumber);
+        void RemoveRental(string movieTitle, string socialSecurityNumber);
+        List<Rental> GetRentalsFor(string socialSecurityNumber);
+    }
+}
